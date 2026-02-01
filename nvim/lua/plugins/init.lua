37991f67dad67833290d9186dd42fdef9acdc3f3
Karaage <karaage01@outlook.jp>
2026-02-01 19:53:45 +0900

    remove nvim tree & add file browser extension

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -106,7 +106,8 @@ require("lazy").setup({
 		},
 	},
 	{
-		"nvim-tree/nvim-tree.lua",
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 	},
 	{
 		"williamboman/mason.nvim",
@@ -241,6 +242,7 @@ require("lazy").setup({
 					},
 				},
 			})
+			require("telescope").load_extension("file_browser")
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
